e07031bd302815f0b8981d4fc0fae009489fe80e
Aditya Bairy <58874927+BattleHoundoom@users.noreply.github.com>
2025-01-02 01:55:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var member = new Object();
+    member.name = "Sunny";
+    member.age = 27;
+    member.skills = ["JavaScript", "HTML", "CSS"];
+    return member;
+}
